73835dc4364baa3178d0a3e31beeddd64f61e770
cviaud <cviaud@gmail.com>
2024-08-04 01:51:27 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <fragment>
+      <h1>Funventory</h1>
+      <h2>Non, si ...</h2>
+      <ol>
+        <li>un</li>
+        <li>deux</li>
+        <li>...</li>
+      </ol>
+    </fragment>
+  );
+}
+
+export default App;
